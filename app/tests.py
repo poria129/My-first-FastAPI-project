92889e0982dccd0061eb4be54c1a058b13f1ce65
poria129 <89725449+poria129@users.noreply.github.com>
2023-10-23 15:47:14 +0330

    add tests.py
     add tests.py for showing an example

---
+++ app/tests.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+
+
+class MongoDBConnection:
+    def __init__(self, host, port, database_name):
+        self.host = host
+        self.port = port
+        self.database_name = database_name
+        self.client = None
+
+    def __enter__(self):
+        self.client = MongoClient(self.host, self.port)
+        self.db = self.client[self.database_name]
+        return self.db
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.client:
+            self.client.close()
+
+
+def get_todo_collection():
+    with MongoDBConnection("localhost", 27017, "todo_app") as db:
+        return db["todos"]
+
+
+def get_user_collection():
+    with MongoDBConnection("localhost", 27017, "todo_app") as db:
+        return db["users"]
+
+
+# Usage example:
+todo_collection = get_todo_collection()
+user_collection = get_user_collection()
